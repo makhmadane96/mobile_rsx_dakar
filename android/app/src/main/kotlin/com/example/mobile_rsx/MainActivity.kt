231a2d5c3bef2e90f3848ad509c82632adf5cbb4
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2024-03-04 21:02:24 +0100

    initialize project

---
+++ android/app/src/main/kotlin/com/example/mobile_rsx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_rsx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      textDirection: TextDirection.ltr,
+      children: [
+      Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.green,Colors.white]),
+            shape: BoxShape.circle
+        ),
+        child: Text("Free Senegal",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+        Container(
+          alignment: Alignment.center,
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.green,Colors.white]),
+              shape: BoxShape.circle
+          ),
+          child: Text("Free Senegal",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      Container(
+        height: 100,
+        width: 100,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.green,Colors.blue]),
+            shape: BoxShape.circle
+        ),
+        child: Text("Free Senegal",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    ],)
+
+  );
+      }
+     /* Container(
+      color: Colors.white,
+      alignment: Alignment(0.0,0.0),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.green,Colors.deepOrange]),
+                shape: BoxShape.circle
+        ),
+      //margin: EdgeInsets.only(top: 100,right: 100),
+      padding: EdgeInsets.only(left: 100),
+      child: Container(
+        color: Colors.blue,
+        constraints: BoxConstraints(
+          maxHeight: 100,
+          maxWidth: 100,
+          minHeight: 50,
+          minWidth: 50
+        ),
+    /*const Text("Free Senegal ",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 40,color: Colors.amberAccent),*/
+      ),
+  ));*/
+
+    //Image.asset('assets/images/photo.jpg'),
+      /*Image.network(
+          "https://i.ibb.co/HG70pXB/image4.jpg",
+          height: 150.0,
+      )*/
+
+
+   /* Text.rich(
+      TextSpan(
+        text: 'Bonjour ',
+        style: TextStyle(color: Colors.pink),
+        children: [
+          TextSpan(text: 'chers ',style: TextStyle(color:Colors.amberAccent)),
+          TextSpan(text: 'apprenants',style:TextStyle(color: Colors.blue) )
+        ]
+      ),
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 30),
+    ),*/
+
+   /* Text(
+      "Hello word world ",
+     // softWrap: false,
+      maxLines: 3,
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize:180,
+        fontStyle: FontStyle.italic,
+        backgroundColor: Colors.pink,
+        decoration: TextDecoration.underline,
+        decorationColor: Colors.amber,
+        decorationStyle: TextDecorationStyle.solid,
+        decorationThickness: 3
+
+      ),
+    ),*/
+
